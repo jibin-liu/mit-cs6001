0cdbb3ba831efd1488be1b414178eb5f4d4cc573
liujibin320 <liujibin320@gmail.com>
2016-10-24 22:08:02 -0700

    add positive decimal support

--- week2/calculate_power_root.py
+++ week2/calculate_power_root.py
@@ -1,25 +1,28 @@
 """
-Use bisection search to calculate square root of a given number x
+Use bisection search to calculate power root of a given number x
 """
 
 
-def calculate_square_root(x, power=2):
-    low = 0
-    high = x
-    guess = (high + low) / 2
-    limit = 0.001
-    guess_number = 0
-
-    while abs(guess ** power - x) >= limit:
-        if guess ** power > x:
-            high = guess
-        else:
-            low = guess
-
+def calculate_power_root(x, power=2):
+    if x == 0:
+        raise ValueError('input is invalid')
+    elif x > 0:
+        low = 0
+        high = x if x > 1 else 1
         guess = (high + low) / 2
-        guess_number += 1
+        limit = 0.00001
+        guess_number = 0
 
-    print('Guess Number is %d' % guess_number)
+        while abs(guess ** power - x) >= limit:
+            if guess ** power > x:
+                high = guess
+            else:
+                low = guess
+
+            guess = (high + low) / 2
+            guess_number += 1
+
+    print('Number of guess is %d' % guess_number)
 
     return guess
 
--- week2/test_calculate_power_root.py
+++ week2/test_calculate_power_root.py
@@ -1,20 +1,33 @@
 """
-tests for calculate_square_root function
+tests for calculate_power_root function
 """
 
-from calculate_square_root import calculate_square_root
+from calculate_power_root import calculate_power_root
 import pytest
 import random
 import math
 
 
 class TestSquareRootCalculator(object):
+    def test_zero(self):
+        with pytest.raises(ValueError):
+            calculate_power_root(0)
+
     def test_constant_integer(self):
-        assert calculate_square_root(4) == 2
+        assert calculate_power_root(4, 2) == 2
 
     def test_integer_greater_than_one(self):
-        x = random.randint(1, 10000)
-        assert abs(calculate_square_root(x) - math.sqrt(x)) <= 0.0001
+        x = random.randint(1, 1000000)
+        p = random.randint(1, 100)
+        assert abs(calculate_power_root(x, p) - x ** (1 / p)) <= 0.00001
+
+    def test_contant_positive_decimal(self):
+        assert calculate_power_root(0.25, 2) == 0.5
+
+    def test_between_zero_and_one(self):
+        x = random.random()
+        p = random.randint(1, 100)
+        assert abs(calculate_power_root(x, p) - x ** (1 / p)) <= 0.0001
 
 
 if __name__ == '__main__':
