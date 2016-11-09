965ade741e7ce45f5452b821bac81bbbdf3cbc35
liujibin320 <liujibin320@gmail.com>
2016-11-08 23:06:13 -0800

    add random_sort

--- week6/search_and_sorting.py
+++ week6/search_and_sorting.py
@@ -10,6 +10,8 @@ implementation of different search and sorting techniques
 Assume L is a list of length n, e is the element to be find
 """
 
+import random
+
 
 def linear_search(L, e):
     """
@@ -40,3 +42,21 @@ def bisection_search(L, e):
             return bisection_search(L[:mid], e)
         else:
             return bisection_search(L[mid + 1:], e)
+
+
+def random_sort(L):
+    """
+    if L is not sorted, shuffle it randomly.
+    """
+    is_sorted = False
+
+    while not is_sorted:
+        random.shuffle(L)
+        is_sorted = True
+
+        for i in range(1, len(L)):
+            if L[i] < L[i - 1]:
+                is_sorted = False
+                break
+
+    return L
